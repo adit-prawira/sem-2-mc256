3db7fac77d70d7d3cf15942cd7688e962170c279
Aditya Prawira <adityaprawira96@gmail.com>
2021-04-12 16:15:26 +1000

    Finish testing simulation

--- OENG1116/assignment1-pt1/task18.m
+++ OENG1116/assignment1-pt1/task18.m
@@ -3,15 +3,23 @@ a = 0;
 c = 0;
 b = 2020;
 messages = "";
+found = 0;
 for i = 1:10000
     for j = 1:10000
         if(i < b && i < j && j > b && (i^2+b^2==j^2))
             a = i;
             c = j;
             messages = sprintf("a = %d, c = %d\n", a,c);
+            found = 1;
             break;
         end    
+        if(found == 1)
+            fprintf("%$d", found);
+        break;
     end
+    end
+    
+    
 end
 
 fprintf("%s\n", messages);
